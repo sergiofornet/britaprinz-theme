16c8628fa66c8681e7ea4b98fffcdd0902ec7faf
sergiofornet <sergio.fornet@gmail.com>
2020-10-22 19:17:07 +0200

    Fix scripts path

--- inc/customizer.php
+++ inc/customizer.php
@@ -56,6 +56,6 @@ function britaprinz_theme_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function britaprinz_theme_customize_preview_js() {
-	wp_enqueue_script( 'britaprinz-theme-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'britaprinz-theme-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), BRITAPRINZ_THEME_VERSION, true );
 }
 add_action( 'customize_preview_init', 'britaprinz_theme_customize_preview_js' );
