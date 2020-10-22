690a1d0daa02bb027c6f9d9c9f3b9de8ea1ea374
sergiofornet <sergio.fornet@gmail.com>
2020-10-22 19:13:04 +0200

    Remane theme version constant

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@
  * @package Brita_Prinz_Theme
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if ( ! defined( 'BRITAPRINZ_THEME_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( 'BRITAPRINZ_THEME_VERSION', '1.0.0' );
 }
 
 if ( ! function_exists( 'britaprinz_theme_setup' ) ) :
@@ -140,10 +140,10 @@ add_action( 'widgets_init', 'britaprinz_theme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function britaprinz_theme_scripts() {
-	wp_enqueue_style( 'britaprinz-theme-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'britaprinz-theme-style', get_stylesheet_uri(), array(), BRITAPRINZ_THEME_VERSION );
 	wp_style_add_data( 'britaprinz-theme-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'britaprinz-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'britaprinz-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), BRITAPRINZ_THEME_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
