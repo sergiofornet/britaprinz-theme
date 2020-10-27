61d0c88555874a9a655b039536a193c86903f25f
sergiofornet <sergio.fornet@gmail.com>
2020-10-27 12:08:38 +0100

    Add template functions

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,167 @@ function britaprinz_theme_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'britaprinz_theme_pingback_header' );
+
+
+/**
+ * Add i18n suffix to custom fields on option pages.
+ */
+function britaprinz_get_i18n_suffix() {
+    $suffix = '';
+    if ( ! defined( 'ICL_LANGUAGE_CODE' ) ) {
+        return $suffix;
+    }
+    $suffix = '_' . ICL_LANGUAGE_CODE;
+    return $suffix;
+}
+
+function britaprinz_get_i18n_theme_option( $option_name ) {
+    $suffix = britaprinz_get_i18n_suffix();
+    return carbon_get_theme_option( $option_name . $suffix );
+}
+
+/**
+ * Disable support for comments and trackbacks in post types.
+ */ 
+add_action( 'admin_init', 'britaprinz_disable_comments_post_types_support' );
+
+function britaprinz_disable_comments_post_types_support() {
+	$post_types = get_post_types();
+	foreach ( $post_types as $post_type ) {
+		if( post_type_supports( $post_type, 'comments' ) ) {
+			remove_post_type_support( $post_type, 'comments' );
+			remove_post_type_support( $post_type, 'trackbacks' );
+		}
+	}
+}
+
+/**
+ * Close comments on the front-end.
+ */ 
+add_filter( 'comments_open', 'britaprinz_disable_comments_status', 20, 2);
+add_filter( 'pings_open', 'britaprinz_disable_comments_status', 20, 2 );
+
+function britaprinz_disable_comments_status() {
+	return false;
+}
+
+
+/**
+ * Hide existing comments.
+ */ 
+add_filter( 'comments_array', 'britaprinz_disable_comments_hide_existing_comments', 10, 2 );
+
+function britaprinz_disable_comments_hide_existing_comments( $comments ) {
+	$comments = array();
+	return $comments;
+}
+
+
+/**
+ * Remove comments page in menu.
+ */
+add_action( 'admin_menu', 'britaprinz_disable_comments_admin_menu' );
+
+function britaprinz_disable_comments_admin_menu() {
+	remove_menu_page( 'edit-comments.php' );
+}
+
+
+/**
+ * Redirect any user trying to access comments page.
+ */
+add_action( 'admin_init', 'britaprinz_disable_comments_admin_menu_redirect' );
+
+function britaprinz_disable_comments_admin_menu_redirect() {
+	global $pagenow;
+	if ( $pagenow === 'edit-comments.php' ) {
+		wp_redirect( admin_url() ); exit;
+	}
+}
+
+/**
+ * Remove comments metabox from dashboard.
+ */
+add_action( 'admin_init', 'britaprinz_disable_comments_dashboard' );
+
+function britaprinz_disable_comments_dashboard() {
+	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+}
+
+
+/**
+ * Remove comments links from admin bar.
+ */
+add_action( 'init', 'britaprinz_disable_comments_admin_bar' );
+
+function britaprinz_disable_comments_admin_bar() {
+	if ( is_admin_bar_showing() ) {
+		remove_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
+	}
+}
+
+/**
+ * Disable html in comments.
+ */
+add_filter( 'pre_comment_content', 'esc_html' );
+
+
+/**
+ * Remove meta tags from wp_header.
+ */
+remove_action( 'wp_head', 'wp_generator' ) ;
+remove_action( 'wp_head', 'wlwmanifest_link' ) ;
+remove_action( 'wp_head', 'rsd_link' ) ;
+
+
+/**
+ * Remove RSS feeds.
+ */
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+
+
+/**
+ * Disable login hints.
+ */
+add_filter( 'login_errors', 'britaprinz_login_errors' );
+
+function britaprinz_login_errors() {
+	return 'nop';
+}
+
+/** 
+ * Disable url guess.
+ */
+add_filter( 'redirect_canonical', 'britaprinz_stop_guessing' );
+
+function britaprinz_stop_guessing( $url ) {
+	if ( is_404() ) {
+		return false;
+	}
+	return $url;
+}
+
+/**
+ * Remove WP emojis.
+ */
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+
+/**
+ * Disable access to author pages
+ */
+add_action( 'template_redirect', 'britaprinz_disable_author_page');
+
+function britaprinz_disable_author_page() {
+
+    global $wp_query;
+
+    if ( is_author() ) {
+
+        // show a 404 page
+        $wp_query->set_404();
+		status_header( 404 );
+
+    }
+}
