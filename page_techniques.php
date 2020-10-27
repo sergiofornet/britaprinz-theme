eb9f2afda3e1a2db5355d1c0658f2d7a2b824482
sergiofornet <sergio.fornet@gmail.com>
2020-10-27 18:30:50 +0100

    Add template for 'techniques' page

---
+++ page_techniques.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Técnicas de grabado
+ *
+ * Template for 'techniques' page
+ *
+ * @package Brita_Prinz_Theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			
+			the_post();
+
+			$args = array(
+				'post_type'   => 'technique',
+				'order'       => 'ASC',
+				'orderby'     => 'title'
+			);
+
+			get_template_part( 'template-parts/content', 'page-techniques', $args );
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
