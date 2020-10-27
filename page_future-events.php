3d85d50e6208f943de32b56a63dbc6090d00072f
sergiofornet <sergio.fornet@gmail.com>
2020-10-27 12:33:54 +0100

    Add templates for Events listing pages

---
+++ page_future-events.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template Name: Exposiciones futuras
+ *
+ * Template for future Events page
+ *
+ * @package Brita_Prinz_Theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			$today = date('Y-m-d');
+
+			$args = array(
+				'post_type'   => 'event',
+				'order'       => 'ASC',
+				'orderby'     => 'start_date',
+				'meta_query'  => array(
+					'start_date' 	=> array(
+						'key'     => 'bp_event_start',
+						'value'   => $today,
+						'compare' => '>',
+						'type'    => 'DATE'
+					),
+				),
+			);
+
+			get_template_part( 'template-parts/content', 'page-exhibition', $args );
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
---
+++ page_past-events.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template Name: Exposiciones pasadas
+ *
+ * Template for past Events page
+ *
+ * @package Brita_Prinz_Theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			
+			the_post();
+
+			$today = date( 'Y-m-d' );
+
+			$args = array(
+				'post_type'   => 'event',
+				'order'       => 'ASC',
+				'orderby'     => 'start_date',
+				'meta_query'  => array(
+					'end_date' => array(
+						'key'     => 'bp_event_end',
+						'value'   => $today,
+						'compare' => '<',
+						'type'    => 'DATE'
+					),
+				),
+			);
+
+			get_template_part( 'template-parts/content', 'page-exhibition', $args );
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
