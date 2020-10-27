d77b3f2534524f241b2e19891dc11a3dc3bb6cd0
sergiofornet <sergio.fornet@gmail.com>
2020-10-27 11:50:08 +0100

    Modify featured image position

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,13 +11,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php 
+		the_title( '<h1 class="entry-title">', '</h1>' ); 
+		
+		britaprinz_theme_post_thumbnail(); 
+		?>
 	</header><!-- .entry-header -->
 
-	<?php britaprinz_theme_post_thumbnail(); ?>
 
 	<div class="entry-content">
-		<?php
+		<?php 
 		the_content();
 
 		wp_link_pages(
