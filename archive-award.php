460a1b73b14bf0db339be5d49144b6f1c91390ab
sergiofornet <sergio.fornet@gmail.com>
2020-10-28 09:11:16 +0100

    Remove unnecesary Artist query

--- archive-award.php
+++ archive-award.php
@@ -26,42 +26,6 @@ get_header();
 			</header><!-- .page-header -->
 
 			<?php
-			// Artist terms query.
-			// $args = array(
-			// 	'taxonomy'		=> 'artist',
-			// 	'orderby'		=> 'order_name',
-			// 	'order'			=> 'ASC',
-			// 	'hide_empty'	=> false,
-			// 	'meta_query'	=> array(
-			// 		'order_name'	=> array(
-			// 			'key'		=> 'bp_artist_order_name',
-			// 			'compare'	=> 'EXISTS',
-			// 		),
-			// 	),
-			// );
-
-			// $artists_query = new WP_Term_Query( $args );
-			
-			if ( ! empty( $artists_query ) && ! is_wp_error( $artists_query ) ) :
-				foreach ( $artists_query->get_terms() as $term ) :
-					?>
-
-						<p>
-							<a href="<?php echo esc_url( get_term_link($term->term_id) ); ?>">
-
-								<?php echo esc_html( $term->name ); ?>
-
-							</a>
-						</p>
-						<p><?php echo esc_html( $term->description ); ?></p>
-
-					<?php
-				endforeach;
-			endif;
-			wp_reset_postdata(); 
-			?>
-
-			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
