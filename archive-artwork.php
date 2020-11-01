92d9bd17ed803b3c45d95be8100b9e1aa22be428
sergiofornet <sergio.fornet@gmail.com>
2020-11-01 18:58:23 +0100

    Add artworks-container

--- archive-artwork.php
+++ archive-artwork.php
@@ -8,6 +8,7 @@
  */
 
 get_header();
+
 ?>
 
 	<main id="primary" class="site-main">
@@ -25,64 +26,72 @@ get_header();
 
 			</header><!-- .page-header -->
 
-			<?php
-			// Artist terms query.
-			$args = array(
-				'taxonomy'		=> 'artist',
-				'orderby'		=> 'order_name',
-				'order'			=> 'ASC',
-				'hide_empty'	=> false,
-				'meta_query'	=> array(
-					'order_name'	=> array(
-						'key'		=> 'bp_artist_order_name',
-						'compare'	=> 'EXISTS',
-					),
-				),
-			);
+			<div style="display: grid; grid-template-columns: 1fr 1fr;">
+				<div>
+				
+					<?php
+					// Artist terms query.
+					$args = array(
+						'taxonomy'		=> 'artist',
+						'orderby'		=> 'order_name',
+						'order'			=> 'ASC',
+						'hide_empty'	=> false,
+						'meta_query'	=> array(
+							'order_name'	=> array(
+								'key'		=> 'bp_artist_order_name',
+								'compare'	=> 'EXISTS',
+							),
+						),
+					);
+					
+					$artists_query = new WP_Term_Query( $args );
+					
+					if ( ! empty( $artists_query ) && ! is_wp_error( $artists_query ) ) :
+						foreach ( $artists_query->get_terms() as $term ) :
+							?>
 
-			$artists_query = new WP_Term_Query( $args );
-			
-			if ( ! empty( $artists_query ) && ! is_wp_error( $artists_query ) ) :
-				foreach ( $artists_query->get_terms() as $term ) :
+								<p>
+									<a class="artist__link" href="<?php echo esc_url( get_term_link($term->term_id) ); ?>" data-artist="<?php echo esc_attr( $term->term_id ); ?>">
+
+										<?php echo esc_html( $term->name ); ?>
+
+									</a>
+								</p>
+								<p><?php echo esc_html( $term->description ); ?></p>
+
+							<?php
+						endforeach;
+					endif;
+					wp_reset_postdata(); 
 					?>
 
-						<p>
-							<a href="<?php echo esc_url( get_term_link($term->term_id) ); ?>">
-
-								<?php echo esc_html( $term->name ); ?>
-
-							</a>
-						</p>
-						<p><?php echo esc_html( $term->description ); ?></p>
-
 					<?php
-				endforeach;
-			endif;
-			wp_reset_postdata(); 
-			?>
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
+						
+						/*
+						* Include the Post-Type-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						*/
+						get_template_part( 'template-parts/content', get_post_type() );
+						
+					endwhile;
+					?>
+				</div>
+				<div class="artworks__container"></div>
+			</div>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			// the_posts_navigation();
-
+		<?php
+		// the_posts_navigation();
 		else :
 
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif;
 		?>
+
 
 	</main><!-- #main -->
 
