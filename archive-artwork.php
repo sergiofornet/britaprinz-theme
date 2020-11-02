6a7ca6a604f1222c5b899efdf4dbe1a665f2b035
sergiofornet <sergio.fornet@gmail.com>
2020-11-02 22:42:01 +0100

    Add artworks__gallery container

--- archive-artwork.php
+++ archive-artwork.php
@@ -66,21 +66,22 @@ get_header();
 
 					<?php
 					/* Start the Loop */
-					while ( have_posts() ) :
-						the_post();
+					// while ( have_posts() ) :
+					// 	the_post();
 						
-						/*
-						* Include the Post-Type-specific template for the content.
-						* If you want to override this in a child theme, then include a file
-						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-						*/
-						get_template_part( 'template-parts/content', get_post_type() );
+					// 	/*
+					// 	* Include the Post-Type-specific template for the content.
+					// 	* If you want to override this in a child theme, then include a file
+					// 	* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					// 	*/
+					// 	get_template_part( 'template-parts/content', get_post_type() );
 						
-					endwhile;
+					// endwhile;
 					?>
 				</div>
 				<div class="artworks__container"></div>
 			</div>
+			<div class="artworks__gallery"></div>
 
 		<?php
 		// the_posts_navigation();
