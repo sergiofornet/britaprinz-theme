082c4d28e98365c5f66745db3b855067ae0dd89f
sergiofornet <sergio.fornet@gmail.com>
2020-11-04 18:22:20 +0100

    Fix coding standadrds

--- assets/js/ajax/artworks-list.js
+++ assets/js/ajax/artworks-list.js
@@ -1,14 +1,14 @@
 function artworksList (artworks) {
-	let html = ``;
-	if ( typeof artworks === 'object' ) {
-		html += `<ul class="artwork-list">`;
+	let html = '';
+	if (typeof artworks === 'object') {
+		html += '<ul class="artwork-list">';
 		artworks.forEach( (element) => {
-			const {featured_techniques, other_techniques} = element.artwork_techniques;
+			const { featured_techniques: featuredTechniques, other_techniques: otherTechniques } = element.artwork_techniques;
 			html += `
 			<li class="artwork" key="artwork-${ element.slug }">
 				<div class="artwork__title"><a href="${ element.link }">${ element.title.rendered }</a></div>
 				<div class="artwork__stuff">
-					${ element.artwork_image_src ? `<div class="artwork__thumbnail"><a href="${ element.link }" data-artwork="${element.id}">${ element.artwork_image_src }</a></div>` : '' }
+					${ element.artwork_image_src ? `<div class="artwork__thumbnail"><a href="${ element.link }" data-artwork="${ element.id }">${ element.artwork_image_src }</a></div>` : '' }
 					<div class="artwork__info">${ element.bp_artwork_year }</div>
 					<div class="artwork__info">${ element.bp_artwork_condition }</div>
 					<div class="artwork__info">${ element.bp_artwork_copy }</div>
@@ -17,15 +17,15 @@ function artworksList (artworks) {
 					${ element.artwork_loan ? `<div class="artwork__info">${ element.artwork_loan }</div>` : '' }
 					${ element.artwork_sale ? `<div class="artwork__info">${ element.artwork_sale }</div>` : '' }
 					<div class="artwork__techniques">
-						${ featured_techniques.map(technique => `<a href="${technique[1]}">${technique[0]}</a>`).join('\n') }
-						${ other_techniques && `<span>${other_techniques}</span>` }
+						${ featuredTechniques.map( (technique) => `<a href="${ technique[ 1 ] }">${ technique[ 0 ] }</a>` ).join( '\n' ) }
+						${ otherTechniques && `<span>${ otherTechniques }</span>` }
 					</div>
 					<div class="artwork__info">${ element.bp_artwork_info }</div>
 				</div>
 			</li>
 			`;
-		} );
-		html += `</ul>`;
+		});
+		html += '</ul>';
 	} else {
 		html = artworks;
 	}
