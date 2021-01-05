f509622aa3033d38347ae785aa55e954bd22c250
sergiofornet <sergio.fornet@gmail.com>
2021-01-05 08:53:26 +0100

    Add conditional rendering to home featured content

--- template-parts/content-front-page.php
+++ template-parts/content-front-page.php
@@ -54,21 +54,27 @@
 					$featured_content = carbon_get_the_post_meta('bp_home_featured');
 
 					foreach ( $featured_content as $content ) :
-						?>
-
-						<div>
-
-							<?php
-							echo sprintf(
-								'<a href="%s">%s</a>',
-								esc_url( get_permalink( $content['bp_home_featured_post'][0]['id'] ) ),
-								esc_html( $content['bp_home_featured_label'] )
-							);
+						if ( $content['bp_home_featured_post'] ) :
+							$content_url = get_permalink( $content['bp_home_featured_post'][0]['id'] );
+							$content_label = $content['bp_home_featured_label'] ?
+								$content['bp_home_featured_label'] :
+								get_the_title( $content['bp_home_featured_post'][0]['id'] );
 							?>
 
-						</div>
+							<div>
 
-						<?php
+								<?php
+								echo sprintf(
+									'<a href="%s">%s</a>',
+									esc_url( $content_url ),
+									esc_html( $content_label )
+								);
+								?>
+
+							</div>
+
+							<?php
+						endif;
 					endforeach;
 				?>
 			
