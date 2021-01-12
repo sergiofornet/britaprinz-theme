a15ea8bcd91886c131f66c62d3506b5a533045f4
sergiofornet <sergio.fornet@gmail.com>
2021-01-12 12:46:12 +0100

    Modify artwork info order

--- assets/js/ajax/artworks-list.js
+++ assets/js/ajax/artworks-list.js
@@ -10,17 +10,17 @@ function artworksList (artworks) {
 				<div class="artwork__info">
 					${ element.artwork_image_src ? `<div class="artwork__thumbnail"><a href="${ element.link }" data-artwork="${ element.id }">${ element.artwork_image_src }</a></div>` : '' }
 					<div class="artwork__year">${ element.bp_artwork_year }</div>
-					<div class="artwork__condition">${ element.bp_artwork_condition }</div>
 					<div class="artwork__copy">${ element.bp_artwork_copy }</div>
-					<div class="artwork__paper">${ element.bp_artwork_paper }</div>
 					<div class="artwork__size">${ element.bp_artwork_size }</div>
-					${ element.artwork_loan ? `<div class="artwork__loan">${ element.artwork_loan }</div>` : '' }
-					${ element.artwork_sale ? `<div class="artwork__sale">${ element.artwork_sale }</div>` : '' }
+					<div class="artwork__paper">${ element.bp_artwork_paper }</div>
+					<div class="artwork__condition">${ element.bp_artwork_condition }</div>
+					${ element.artwork_info ? `<div class="artwork__description">${ element.artwork_info }</div>` : '' }
 					<div class="artwork__techniques">
 						${ featuredTechniques.map( (technique) => `<a class="technique" href="${ technique[ 1 ] }">${ technique[ 0 ] }</a>` ).join( '\n' ) }
 						${ otherTechniques && `<span>${ otherTechniques }</span>` }
 					</div>
-					${ element.artwork_info ? `<div class="artwork__description">${ element.artwork_info }</div>` : '' }
+					${ element.artwork_loan ? `<div class="artwork__loan">${ element.artwork_loan }</div>` : '' }
+					${ element.artwork_sale ? `<div class="artwork__sale">${ element.artwork_sale }</div>` : '' }
 				</div>
 			</li>
 			`;
