f8ec2b57504995825852357fd7f01fc8bdc1d18a
sergiofornet <sergio.fornet@gmail.com>
2021-05-28 17:01:14 +0200

    Add archive project template

---
+++ archive-project.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The template for displaying Project archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Brita_Prinz_Theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php 
+		$bpa_theme_archive_project_post_type = get_post_type();
+		get_template_part( 'template-parts/nav/secondary', '', "{$bpa_theme_archive_project_post_type}-menu" );
+		
+		if ( have_posts() ) :
+			?>
+
+			<header class="page-header">
+				<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php 
+		
+			echo apply_filters( 'the_content', ( britaprinz_get_i18n_theme_option( 'bp_technique_history' ) ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			?>
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
