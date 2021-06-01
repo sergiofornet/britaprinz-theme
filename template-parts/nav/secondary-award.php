816ccf77f2bffcb151c17011c650f7628e66b897
sergiofornet <sergio.fornet@gmail.com>
2021-06-01 16:31:17 +0200

    Modify secondary nav markup

--- template-parts/nav/secondary-award.php
+++ template-parts/nav/secondary-award.php
@@ -9,23 +9,17 @@
 
 // $args -> menu theme location passed in by get_template_part 
 if ( 'undefined' !== $args && has_nav_menu( $args ) ) :
-	?>
-
-	<nav class="secondary">
-
-		<?php 
 		wp_nav_menu(
 			array(
-				'theme_location'  => $args,
-				'container_class' => "menu-{$args}-container",
-				'menu_class'      => 'sidebar-menu',
-				'walker'          => ( new Bpa_Theme_Walker_Nav_Menu ),
+				'container'            => 'nav',
+				'container_id'         => "{$args}-container",
+				'container_class'      => "{$args}-container secondary-navigation",
+				'container_aria_label' => 'Secondary navigation',
+				'menu_id'              => "{$args}",
+				'menu_class'           => "{$args} menu nav-menu",
+				'fallback_cb'          => false,
+				'walker'               => new Bpa_Theme_Walker_Nav_Menu(),
+				'theme_location'       => $args,
 			)
 		);
-		?>
-
-	</nav>
-
-	<?php
 endif;
-
