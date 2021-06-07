9027ef84926593df0b7f2ad874fe502679e3d553
sergiofornet <sergio.fornet@gmail.com>
2021-06-07 19:26:02 +0200

    Add active/inactive state to buttons

--- assets/js/award/showAwardInfo.js
+++ assets/js/award/showAwardInfo.js
@@ -14,22 +14,63 @@ import { asyncFetch } from '../util/async-fetch';
 function showAwardInfo(triggers, target, ajaxUrl, lang, options, callback) {
 	const awardHtml = '';
 	triggers.forEach((button) => {
+		button.setAttribute('aria-pressed', 'false');
 		button.addEventListener(
 			'click',
 			(event) => {
-				target.classList.add('loading');
-				asyncFetch(
-					`${ajaxUrl}/${event.currentTarget.dataset.edition}`,
-					options
-				).then((jsonResponse) => {
-					target.innerHTML = '';
-					target.insertAdjacentHTML(
-						'afterbegin',
-						callback(jsonResponse, lang)
+				target.classList.contains('loaded')
+					? target.classList.replace('loaded', 'loading')
+					: target.classList.add('loading');
+				// Check if button is inactive
+				if (
+					event.currentTarget.classList.contains(
+						'edition-item__button--inactive'
+					)
+				) {
+					// Search for active button
+					if (
+						document.querySelector('.edition-item__button--active')
+					) {
+						// Make active button inactive
+						document
+							.querySelector('.edition-item__button--active')
+							.classList.replace(
+								'edition-item__button--active',
+								'edition-item__button--inactive'
+							);
+					}
+					// Fetch AJAX data
+					asyncFetch(
+						`${ajaxUrl}/${event.currentTarget.dataset.edition}`,
+						options
+					).then((jsonResponse) => {
+						target.innerHTML = '';
+						target.insertAdjacentHTML(
+							'afterbegin',
+							callback(jsonResponse, lang)
+						);
+						// Toggle target loading state
+						target.classList.contains('loading') &&
+							target.classList.replace('loading', 'loaded');
+					});
+					// Make current button active
+					event.currentTarget.classList.replace(
+						'edition-item__button--inactive',
+						'edition-item__button--active'
 					);
+					button.setAttribute('aria-pressed', 'true');
+				} else {
+					// If pressed button is already active
+					target.innerHTML = '';
+					// Toggle target loading state
 					target.classList.contains('loading') &&
 						target.classList.replace('loading', 'loaded');
-				});
+					event.currentTarget.classList.replace(
+						'edition-item__button--active',
+						'edition-item__button--inactive'
+					);
+					button.setAttribute('aria-pressed', 'false');
+				}
 			},
 			false
 		);
