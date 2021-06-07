3c81d43c941bd37d1524d64b768b2cb29e335b28
sergiofornet <sergio.fornet@gmail.com>
2021-06-07 14:02:02 +0200

    Remove no longer needed markup

--- template-parts/content-page-catalogues.php
+++ template-parts/content-page-catalogues.php
@@ -12,56 +12,6 @@
 	get_template_part( 'template-parts/nav/secondary', 'award', 'award-menu' );	
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-	</header><!-- .entry-header -->
-	<div class="entry-content">
-
-		<?php
-		the_content();
-		$award_query = new WP_Query( $args );
-
-		if ( $award_query->have_posts() ) :
-			?>
-
-			<ul>
-
-			<?php
-			while ( $award_query->have_posts() ) :
-				$award_query->the_post();
-				
-				if ( carbon_get_the_post_meta( 'bp_award_catalogue' ) ) :
-					?>
-
-					<li>
-
-						<?php 
-						the_title(
-							sprintf(
-								'<a href="%s" title="%s" target="_blank" rel="noopener noreferrer">',
-								esc_url( wp_get_attachment_url( carbon_get_the_post_meta( 'bp_award_catalogue' ) ) ),
-								esc_html( get_the_title() ) 
-							),
-							'</a>'
-						);
-						?>
-
-					</li>
-
-					<?php
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
-			</ul>
-
-			<?php
-			wp_reset_postdata();
-		endif;
-		?>
-	</div><!-- .entry-content -->
-</article><!-- #post-<?php the_ID(); ?> -->
+<div class="edition-container">
+	<span>EDITION</span>
+</div><!--.edition-container -->
