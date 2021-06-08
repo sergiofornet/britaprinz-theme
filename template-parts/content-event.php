7f3b356efd07e39aa8cc0764823bff1cab89285c
sergiofornet <sergio.fornet@gmail.com>
2021-06-08 14:31:17 +0200

    Add event template part to avoid code repetition

--- template-parts/content-event.php
+++ template-parts/content-event.php
@@ -16,71 +16,7 @@ endif;
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
 
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-		?>
+	<?php get_template_part( 'template-parts/events/event', '' ); ?>
 
-	</header><!-- .entry-header -->
-
-	<?php britaprinz_theme_post_thumbnail(); ?>
-
-	<div class="entry-content">
-
-		<?php 
-		if ( is_singular() ) :
-			?>
-
-			<p><?php echo esc_html( carbon_get_the_post_meta( 'bp_event_artist' ) ); ?></p>
-			<div><?php echo wpautop( carbon_get_the_post_meta( 'bp_event_info' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></div>
-			<p><?php echo esc_html( date( __( 'd/m/Y', 'britaprinz-theme' ), strtotime( carbon_get_the_post_meta( 'bp_event_start' ) ) ) ); ?></p>
-			<p><?php echo esc_html( date( __( 'd/m/Y', 'britaprinz-theme' ), strtotime( carbon_get_the_post_meta( 'bp_event_end' ) ) ) ); ?></p>
-			<p><?php echo esc_html( carbon_get_the_post_meta( 'bp_artwork_condition' ) ); ?></p>
-			<div>
-
-				<?php 
-				$gallery = carbon_get_the_post_meta( 'bp_event_gallery' );
-
-				foreach ( $gallery as $image ) :
-					echo wp_get_attachment_image( $image, 'thumbnail' );
-				endforeach;
-				?>
-
-			</div>
-
-			<?php 
-		endif;
-
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'britaprinz-theme' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'britaprinz-theme' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-
-	</div><!-- .entry-content -->
-	<footer class="entry-footer">
-
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
