c1409c44241e04be9836d5570f6d70e2d14e5033
sergiofornet <sergio.fornet@gmail.com>
2021-06-10 11:15:52 +0200

    Remove post content from archive pages

--- template-parts/content.php
+++ template-parts/content.php
@@ -21,13 +21,16 @@ endif;
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			?>
+			
+			if ( has_excerpt() ) :
+				?>
 
-			<div>
-				<?php the_excerpt(); ?>
-			</div>
+				<div class="excerpt">
+					<?php the_excerpt(); ?>
+				</div>
 		
-			<?php
+				<?php
+			endif;
 		endif;
 
 		if ( 'post' === get_post_type() ) :
@@ -41,33 +44,43 @@ endif;
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php britaprinz_theme_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'britaprinz-theme' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'britaprinz-theme' ),
-				'after'  => '</div>',
-			)
-		);
+	<?php 
+	
+	britaprinz_theme_post_thumbnail(); // must review 
+	
+	if ( is_singular() ) :
 		?>
-	</div><!-- .entry-content -->
+
+			<div class="entry-content">
+				<?php
+				the_content(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'britaprinz-theme' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						wp_kses_post( get_the_title() )
+					)
+				);
+
+				wp_link_pages(
+					array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'britaprinz-theme' ),
+						'after'  => '</div>',
+					)
+				);
+				?>
+			</div><!-- .entry-content -->
+
+		<?php
+	endif;
+	?>
+
 
 	<footer class="entry-footer">
 		<?php britaprinz_theme_entry_footer(); ?>
