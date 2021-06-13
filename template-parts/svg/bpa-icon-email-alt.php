ce589f0d4eb6fe94110f610b846bc397027859b7
sergiofornet <sergio.fornet@gmail.com>
2021-06-13 20:05:21 +0200

    Add inline svg icon templates

---
+++ template-parts/svg/bpa-icon-email-alt.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template part for inline svg amail alt icon
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Brita_Prinz_Theme
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+<svg version="1.0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 612 612"><path d="M82.35 17.339c-41.335 0-64.571 21.37-64.571 65.036v450.122c0 40.843 20.891 62.228 62.192 62.228h452.024c41.303 0 62.226-20.32 62.226-62.228V82.375c0-42.6-20.923-65.036-63.523-65.036 0 0-448.491-.142-448.348 0z" stroke="#fff" stroke-width="3.408"/><g fill="none" stroke="#fff" stroke-width="30.655"><path d="M510.857 445.868V145.054H102.143v300.814h408.714z"/><path d="M105.945 148.886l176.32 178.668c13.069 13.17 36.848 13.014 49.641.12l176.199-177.35M111.999 435.962l135.095-138.106M499.451 434.37l-131.452-134.6"/></g></svg>
