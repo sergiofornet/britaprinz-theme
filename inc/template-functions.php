2e93380fcabb7883d3bfcc8ae56ec8028c21b626
sergiofornet <sergio.fornet@gmail.com>
2021-06-18 18:31:13 +0200

    Modify archive pages titles

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -309,3 +309,26 @@ function britaprinz_theme_get_catalogues() {
 
 	return $catalogues_output;
 }
+
+/**
+ * Modify archives titles
+ * 
+ * @param string $title - the archive title.
+ * @return string $title  
+ */
+function britaprinz_theme_archive_title( $title ) {
+	if ( is_category() ) {
+		$title = single_cat_title( '', false );
+	} elseif ( is_tag() ) {
+		$title = single_tag_title( '', false );
+	} elseif ( is_author() ) {
+		$title = '<span class="vcard">' . get_the_author() . '</span>';
+	} elseif ( is_post_type_archive() ) {
+		$title = post_type_archive_title( '', false );
+	} elseif ( is_tax() ) {
+		$title = single_term_title( '', false );
+	}
+
+	return $title;
+}
+add_filter( 'get_the_archive_title', 'britaprinz_theme_archive_title' );
