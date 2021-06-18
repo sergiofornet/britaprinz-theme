dcd1dbdf9741daaa0fb266cbe72324dd8afd607d
sergiofornet <sergio.fornet@gmail.com>
2021-06-18 18:31:31 +0200

    Remove archive description

--- archive-project.php
+++ archive-project.php
@@ -24,7 +24,6 @@ get_header();
 				<header class="page-header">
 					<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' ); // must review
 					?>
 				</header><!-- .page-header -->
 
