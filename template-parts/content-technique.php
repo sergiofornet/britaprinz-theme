cf9e0add8ac1a91c4a790072f3b4f9a4a3153242
sergiofornet <sergio.fornet@gmail.com>
2021-06-19 18:14:17 +0200

    Add technique related artists markup and stuff

--- template-parts/content-technique.php
+++ template-parts/content-technique.php
@@ -34,6 +34,69 @@
 			wp_kses_post( get_the_title() )
 			)
 		);
+
+		$artworks_link = carbon_get_the_post_meta( 'bp_technique_related_text' ) ? carbon_get_the_post_meta( 'bp_technique_related_text' ) : null;
+		$artworks = carbon_get_the_post_meta( 'bp_technique_artwork' ) ? carbon_get_the_post_meta( 'bp_technique_artwork' ) : null;
+
+
+		if ( $artworks && $artworks_link ) :
+			?>
+
+			<div class="related_artworks">
+				<button class="related-artworks__toggle"><?php echo esc_html( $artworks_link ); ?></button>
+			</div>
+
+			<div class="artwork-gallery hidden">
+				<div class="artwork-gallery__close">
+					<button class="close">&times;</button>
+				</div>
+				<div class="artwork-gallery__slider">
+
+					<?php
+					foreach ( $artworks as $artwork ) :
+						$artwork_id    = $artwork['id'];
+						$artwork_title = get_the_title( $artwork_id );
+						$artwork_year  = carbon_get_post_meta( $artwork_id, 'bp_artwork_year' );
+						$artwork_copy  = carbon_get_post_meta( $artwork_id, 'bp_artwork_copy' );
+						$artwork_image = get_the_post_thumbnail( $artwork_id, 'thumbnail' );
+						
+						$artist        = get_the_terms( $artwork_id, 'artist' );
+						$artist_name   = $artist[0]->name;
+						$artist_link   = get_term_link( $artist[0]->term_id, 'artist' );
+						$artist_slug   = $artist[0]->slug;
+						
+						if ( $artwork_image ) :
+							?>
+							<div>
+							<figure>
+
+								<?php echo $artwork_image; ?>
+
+								<figcaption>
+
+									<?php 
+									echo sprintf(
+										'<strong><a href="%1$s">%2$s</a></strong><br>%3$s<br>%4$s<br>%5$s',
+										esc_url( $artist_link ),
+										esc_html( $artist_name ),
+										esc_html( $artwork_title ),
+										esc_html( $artwork_year ),
+										esc_html( $artwork_copy )
+									);
+									?>
+
+								</figcaption>
+							</figure>
+							</div>
+
+							<?php
+						endif;
+					endforeach;
+					?>
+				</div>
+			</div>
+			<?php
+		endif;
 		?>
 
 	</div><!-- .entry-content -->
