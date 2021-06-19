68c5f994d758b76768ef417d54c05ffa1c684a6b
sergiofornet <sergio.fornet@gmail.com>
2021-06-19 18:10:18 +0200

    Remove no longer needed stuff

--- template-parts/content-technique.php
+++ template-parts/content-technique.php
@@ -9,93 +9,32 @@
 
 <?php 
 	get_template_part( 'template-parts/nav/secondary', '', 'technique-menu' );
-?>
+	?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header class="entry-header technique-header">
 
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-		?>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 	</header><!-- .entry-header -->
+	<div class="entry-content technique-content">
 
-	<?php britaprinz_theme_post_thumbnail(); ?>
-
-	<div class="entry-content">
-
-		<?php 
-		if ( is_singular() ) :
-			the_content(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'britaprinz-theme' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				)
-			);
-
-			$artwork = carbon_get_the_post_meta( 'bp_technique_artwork' ) ? carbon_get_the_post_meta( 'bp_technique_artwork' ) : null;
-			
-			if ( $artwork) :
-				$artwork_id      = $artwork[0]['id'];
-				$artwork_title   = get_the_title( $artwork_id );
-
-				$artwork_gallery = carbon_get_post_meta( $artwork_id, 'bp_artwork_gallery' );
-
-				$artist          = get_the_terms( $artwork_id, 'artist' );
-				$artist_name     = $artist[0]->name;
-				$artist_link     = get_term_link( $artist[0]->term_id, 'artist' );
-				$artist_slug     = $artist[0]->slug;
-
-				?>
-
-				<div class="technique__artwork">
-
-				<?php
-				foreach ( $artwork_gallery as $image ) :
-					?>
-					
-					<figure>
-						<?php echo wp_get_attachment_image( $image, 'thumbnail' ); ?>
-					</figure>
-
-					<?php
-				endforeach;
-				?>
-
-				</div>
-				<p class="technique__artist">
-					<span class="artist__name"><?php esc_html_e( $artwork_title, 'britaprinz-theme' ); ?> - </span>
-					<span class="artist__link">
-						<a href="<?php echo esc_url( $artist_link ); ?>"><?php echo esc_html( $artist_name ); ?></a>
-					</span>
-				</p><!-- Artist -->
-		
-				<?php 
-			endif;
-		endif;
-		
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'britaprinz-theme' ),
-				'after'  => '</div>',
+		<?php
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'britaprinz-theme' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+			wp_kses_post( get_the_title() )
 			)
 		);
 		?>
-	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php britaprinz_theme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	</div><!-- .entry-content -->
 </article><!-- #post-<?php the_ID(); ?> -->
