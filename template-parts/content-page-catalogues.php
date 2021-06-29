fe24dde51d1de17073747ac43c1bf3ca5d24791c
sergiofornet <sergio.fornet@gmail.com>
2021-06-29 13:28:55 +0200

    Fix return-button class

--- template-parts/content-page-catalogues.php
+++ template-parts/content-page-catalogues.php
@@ -13,7 +13,7 @@
 ?>
 
 <div class="award-edition-container" data-state="loaded">
-	<button class="award-edition-container__return-button return button" aria-pressed="false"><</button>
+	<button class="award-edition-container__return-button return-button" aria-pressed="false"><</button>
 	<article id="post-<?php //the_ID(); ?>" <?php //post_class(); ?>>
 		<div class="entry-content">
 
