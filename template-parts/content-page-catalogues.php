dba841acdb8a2fcf2529c016ed038ba60a145c1f
sergiofornet <sergio.fornet@gmail.com>
2021-06-30 09:25:41 +0200

    Add catalogue styles

--- template-parts/content-page-catalogues.php
+++ template-parts/content-page-catalogues.php
@@ -12,7 +12,7 @@
 	get_template_part( 'template-parts/nav/secondary', 'award', 'award-menu' );	
 ?>
 
-<div class="award-edition-container" data-state="loaded">
+<div class="award-edition-container container__catalogue" data-state="loaded">
 	<button class="award-edition-container__return-button return-button" aria-pressed="false"><</button>
 	<article id="post-<?php //the_ID(); ?>" <?php //post_class(); ?>>
 		<div class="entry-content catalogue">
