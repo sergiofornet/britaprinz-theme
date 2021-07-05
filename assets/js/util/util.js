a7d0aa1a4305e16dd30869d3a728f90c89dbc2ee
sergiofornet <sergio.fornet@gmail.com>
2021-07-05 21:28:49 +0200

    Add util functions

---
+++ assets/js/util/util.js
@@ -0,0 +1,51 @@
+/**
+ * Handles active/inactive state on buttons
+ *
+ * @param {NodeList} buttons Buttons object
+ * @param {string} activeState Active state class
+ * @param {string} inactiveState Inactive state class
+ * @param target
+ */
+function toggleButtonState(buttons, activeState, inactiveState, target) {
+	buttons.forEach((button) => {
+		button.setAttribute('aria-pressed', 'false');
+		button.addEventListener('click', (event) => {
+			if (event.currentTarget.classList.contains(inactiveState)) {
+				if (document.querySelector(`.${activeState}`)) {
+					document
+						.querySelector(`.${activeState}`)
+						.classList.replace(activeState, inactiveState);
+				}
+				event.currentTarget.classList.replace(
+					inactiveState,
+					activeState
+				);
+				button.setAttribute('aria-pressed', 'true');
+			} else {
+				target.innerHTML = '';
+				event.currentTarget.classList.replace(
+					activeState,
+					inactiveState
+				);
+				button.setAttribute('aria-pressed', 'false');
+			}
+		});
+	});
+}
+
+/**
+ * Get the computed size of an element
+ *
+ * @param {HTMLElement} element - html element
+ * @return {number} element computed height
+ */
+function getHeight(element) {
+	return element.offsetHeight;
+}
+
+function handleVhValue() {
+	const vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+export { toggleButtonState, getHeight, handleVhValue };
