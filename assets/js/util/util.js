ee2620316e52d8243f9fd1c290dfe8db7233b03e
sergiofornet <sergio.fornet@gmail.com>
2021-07-07 09:10:06 +0200

    Create HandleScroll function

--- assets/js/util/util.js
+++ assets/js/util/util.js
@@ -63,4 +63,29 @@ async function asyncCreateImage(src) {
 	});
 }
 
-export { toggleButtonState, getHeight, handleVhValue, asyncCreateImage };
+function HandleScroll() {
+	this.body = document.querySelector('body');
+	this.scrollPosition = 0;
+}
+HandleScroll.prototype.enable = function () {
+	this.scrollPosition = window.pageYOffset;
+	this.body.style.overflow = 'hidden';
+	this.body.style.position = 'fixed';
+	this.body.style.top = `-${this.scrollPosition}px`;
+	this.body.style.width = '100%';
+};
+HandleScroll.prototype.disable = function () {
+	this.body.style.removeProperty('overflow');
+	this.body.style.removeProperty('position');
+	this.body.style.removeProperty('top');
+	this.body.style.removeProperty('width');
+	window.scrollTo(0, this.scrollPosition);
+};
+
+export {
+	toggleButtonState,
+	getHeight,
+	handleVhValue,
+	asyncCreateImage,
+	HandleScroll,
+};
